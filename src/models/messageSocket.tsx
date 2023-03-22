26bdce4bb154134f02e78c6404ab2f645a736e87
T-JK <liushao121@hotmail.com>
2023-03-22 21:52:21 +0800

    feat: 调整websocket 来源

--- src/models/messageSocket.tsx
+++ src/models/messageSocket.tsx
@@ -3,8 +3,8 @@ import { useWebSocket } from 'ahooks';
 
 export default () => {
   const { initialState } = useModel('@@initialState');
-  // const url = location.hostname.replace('localhost', '') || '18.179.207.178';
-  const url = 'localhost:3000';
+  const url = location.hostname.replace('localhost', '') || '18.179.207.178';
+  // const url = 'localhost:3000';
   const { connect, readyState, latestMessage } = useWebSocket(
     `ws://${url}/websocket?userId=${initialState?.currentUser?.userId}`,
     {
