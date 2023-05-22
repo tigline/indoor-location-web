5e9424a029c68624b90ef6a6247b9ed4eee749d7
tigline <projectgame@163.com>
2023-05-22 18:01:50 +0900

    fix not refresh after edit

--- src/pages/warning-manage/board/index.tsx
+++ src/pages/warning-manage/board/index.tsx
@@ -1,15 +1,24 @@
 import { RemoveButtonPopover } from '@/components/remove-button.popover';
 import { deleteAlarm, pageAlarm } from '@/services/swagger/gaojingguanli';
 import { fmt, fmtPage, OK } from '@/utils/global.utils';
-import { PageContainer, ProColumns, ProTable } from '@ant-design/pro-components';
-import { useIntl, useRequest } from '@umijs/max';
+import { ActionType, PageContainer, ProColumns, ProTable } from '@ant-design/pro-components';
+import { useIntl, useModel, useRequest } from '@umijs/max';
 import { Button, notification } from 'antd';
 import dayjs from 'dayjs';
 import { DealAlarmModal } from './components/deal-alarm.modal';
+import { useEffect, useRef } from 'react';
+
 
 export default function Page() {
   const intl = useIntl();
-
+  const actionRef = useRef<ActionType>();
+  const { data } = useModel('messageSocket');
+  useEffect(() => {
+    const res = JSON.parse(data) as ILocation;
+    if (res.type === 'Alarm') {
+      actionRef.current?.reload();
+    }
+  }, [data]);
   const { run: remove, fetches } = useRequest(deleteAlarm, {
     manual: true,
     fetchKey: (o) => o.alarmIds.join('-'),
@@ -25,13 +34,14 @@ export default function Page() {
       }
     },
   });
+
   const columns: ProColumns<API.AlarmInfo>[] = [
     {
       title: intl.formatMessage({
         id: 'pages.system.warning-manage.board.fence',
         defaultMessage: '标签',
       }),
-      dataIndex: 'fenceId',
+      dataIndex: 'deviceId',
     },
     {
       title: intl.formatMessage({
@@ -127,6 +137,8 @@ export default function Page() {
         API.AlarmInfo,
         API.pageAlarmParams & { createTime: [dayjs.ConfigType, dayjs.ConfigType] }
       >
+       options={{ setting: false }}
+       actionRef={actionRef} 
         columns={columns}
         beforeSearchSubmit={(values) => {
           const { createTime, ...rest } = values;
